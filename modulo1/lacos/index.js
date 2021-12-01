e5bc5cfbba97c4caa71e153a649dcba97c046dfb
JorgeSchuck <jorgegschuck@gmail.com>
2021-11-30 21:36:22 -0300

    Entregando exercicio de lacos

---
+++ modulo1/lacos/index.js
@@ -0,0 +1,69 @@
+//Exercícios de interpretação de código
+//1. ele está fazendo loop e adicionando 1 ao valor de cada loop
+// começando na posiçao 0 ele vai somando até 1 numero menor que 5. 
+// 1+ 2+ 3+ 4 e o resultado impresso é 10
+
+//2.
+// a) sera impresso todos os numeros que forem maiores que 18
+//b) 
+// 
+
+//3. seria 4 linhas
+
+//Exercícios de escrita de código
+
+//1.
+
+
+// let bichinhos = Number(prompt("Quantos bichinhos de estimação você tem?"))
+
+// if (bichinhos === 0){
+// 	console.log("Que pena! Você pode adotar um pet!")
+// }else{
+
+// 	let listaBichos = []
+
+// 	for(let i = 0; i < bichinhos; i++){
+// 		listaBichos.push(prompt("Qual o nome deles?"))
+// 	}console.log(listaBichos)
+// } 
+
+//2.
+
+
+// const arrayOriginal = [20, 39, 40, 28, 50]
+//a)
+// for(let i = 0; i < arrayOriginal.length; i++){
+	
+// }console.log(arrayOriginal)
+
+// //b)
+// for(let i = 0; i < arrayOriginal.length; i++){
+// 	console.log(arrayOriginal[i]/10)
+// }
+// //c)
+// const outraArray = []
+// for(let i = 0; i < arrayOriginal.length; i++){
+// 	if(arrayOriginal[i] % 2 === 0){
+// 		outraArray.push(arrayOriginal[i])
+// 	}
+// }console.log(outraArray)
+
+// //d)
+// let array = []
+// for(let i = 0; i < arrayOriginal.length; i++){
+// 	array.push(`O elemento do índex i é: ${arrayOriginal[i]}`)
+// }console.log(array)
+
+// //e)
+// let valorMinimo = 0
+// let valorMaximo = 100
+
+// for(let i = 0; i < arrayOriginal.length; i++){
+// if (arrayOriginal[i] < valorMinimo){
+// 	valorMinimo = arrayOriginal[i]
+// }else if(arrayOriginal[i] > valorMaximo){
+// 	valorMaximo = arrayOriginal[i]
+// }console.log(`O menor numero é: ${valorMinimo} e o maior numero é: ${valorMaximo}`)
+
+// }
