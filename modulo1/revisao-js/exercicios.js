1569dbb99499060e1bc4a6dde1f7ffc824b1f890
JorgeSchuck <jorgegschuck@gmail.com>
2021-12-09 21:32:46 -0300

    fiz os exercicios 11, 12, 13, 13b, 14

--- modulo1/revisao-js/exercicios.js
+++ modulo1/revisao-js/exercicios.js
@@ -22,27 +22,55 @@ function retornaArrayOrdenado(array) {
 
 // EXERCÍCIO 04
 function retornaNumerosPares(array) {
-  return
+  var pares = array.filter(par =>(par % 2 === 0))
+  return pares
 }
 
 // EXERCÍCIO 05
 function retornaNumerosParesElevadosADois(array) {
- 
+  let array2 = []
+  for(number of array){
+    if(number % 2 === 0){
+    array2.push(number*number)}
+  }
+  return array2
 }
 
 // EXERCÍCIO 06
 function retornaMaiorNumero(array) {
-  
+  return Math.max.apply(null, array)
 }
 
 // EXERCÍCIO 07
 function retornaObjetoEntreDoisNumeros(num1, num2) {
-
+  let maiorNumero
+  let menorNumero
+  if(num1 > num2){
+    maiorNumero = num1
+    menorNumero = num2
+  }else{
+    maiorNumero = num2
+    menorNumero = num1
+  }
+  let divisor = maiorNumero % menorNumero === 0
+  
+  const objetoEntreDoisNumeros = {
+  maiorNumero: maiorNumero,
+  maiorDivisivelPorMenor: divisor,
+  diferenca: (maiorNumero - menorNumero)
+}
+return objetoEntreDoisNumeros
 }
 
 // EXERCÍCIO 08
 function retornaNPrimeirosPares(n) {
-   
+   let arrayDePares = []
+   for(let num = 0; arrayDePares.length < n; num++){
+     if (num % 2 === 0){
+       arrayDePares.push(num)
+     }
+   }
+   return arrayDePares
 }
 
 // EXERCÍCIO 09
@@ -52,31 +80,39 @@ function classificaTriangulo(ladoA, ladoB, ladoC) {
 
 // EXERCÍCIO 10
 function retornaSegundoMaiorESegundoMenor(array) {
-  
+
 }
 
 // EXERCÍCIO 11
 function retornaChamadaDeFilme(filme) {
-   
+  return `Venha assistir ao filme ${filme.nome}, de ${filme.ano}, dirigido por ${filme.diretor} e estrelado por ${filme.atores.join(", ")}.`
 }
 
 // EXERCÍCIO 12
 function retornaPessoaAnonimizada(pessoa) {
-   
+  novoObjeto = {...pessoa, nome:"ANÔNIMO"}
+  return novoObjeto
 }
 
 // EXERCÍCIO 13A
 function retornaPessoasAutorizadas(pessoas) {
-   
+let pessoasAutorizadas = pessoas.filter(pessoa => (pessoa.altura >= 1.5 && pessoa.idade > 14 && pessoa.idade < 60))
+ return pessoasAutorizadas
 }
 
 // EXERCÍCIO 13B
 function retornaPessoasNaoAutorizadas(pessoas) {
-  
+  let pessoasNaoAutorizadas = pessoas.filter(pessoa => (pessoa.altura <= 1.5 || pessoa.idade <= 14 || pessoa.idade > 60))
+ return pessoasNaoAutorizadas
 }
 
 // EXERCÍCIO 14
 function retornaContasComSaldoAtualizado(contas) {
+  for (obj of contas) { 
+    for (compra of obj.compras) {
+       obj.saldoTotal = obj.saldoTotal - compra } 
+       obj.compras = []
+       } return contas
 
 }
 
