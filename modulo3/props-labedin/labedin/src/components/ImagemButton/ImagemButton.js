ce54670241932416f0bc6d43b0c38c2d172752b4
JorgeSchuck <jorgegschuck@gmail.com>
2022-01-27 21:54:55 -0300

    enviando exercicio de props e componentes react

---
+++ modulo3/props-labedin/labedin/src/components/ImagemButton/ImagemButton.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+
+import styled from 'styled-components'
+
+
+
+const EstiloDiv = styled.div`
+    display: flex;
+    align-items: center;
+    border: 1px solid black;
+    border-radius: 50px;
+    width: 200px;
+    padding: 15px 30px;
+    margin: 10px auto;
+`
+const EstiloImg = styled.img`
+    width: 30px;
+    margin-right: 10px;
+`
+function ImagemButton(props) {
+    return (
+        <EstiloDiv className="image-button-container">
+            <EstiloImg src={ props.imagem }/>
+            <p>{ props.texto }</p>
+        </EstiloDiv>
+
+    )
+}
+
+export default ImagemButton;
