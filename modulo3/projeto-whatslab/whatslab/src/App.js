53cbd814985572a88d9bfa1e9cea12edb7612c9f
JorgeSchuck <jorgegschuck@gmail.com>
2022-02-05 20:55:23 -0300

    Trabalhando no Whatslab

--- modulo3/projeto-whatslab/whatslab/src/App.js
+++ modulo3/projeto-whatslab/whatslab/src/App.js
@@ -1,25 +1,124 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import styled from 'styled-components'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>JOJOJO</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+const Container = styled.div`
+  max-width: 600px;
+    height: 90vh;
+    border: 2px solid black;
+    flex: 1 1 0%;
+    display: flex;
+    flex-direction: column-reverse;
+    margin: auto;
+    background-image: url(https://i.pinimg.com/originals/ef/74/90/ef749024703cafaa02500e31c5c1a0f2.jpg);
+    word-wrap: break-word;
+    border-radius: 15px;
+    margin-top:20px;
+    font-size: 20px;
+
+  
+
+  
+  
+`
+const StyledInputs = styled.input`
+width: 19.5%;
+margin-bottom: 2px;
+border-bottom-left-radius:15px;
+margin-left:6px;
+
+`
+const  StyledInputs1= styled.input`
+width: 58%;
+border: 2px solid;
+`
+const  StyledButton = styled.button`
+width: 17.3%;
+border-bottom-right-radius:15px;
+
+`
+class App extends React.Component {
+
+  
+  state = {
+    
+    postagens: [],
+
+    valorInputUsuario: "",
+    valorInputMensagem: "",
+    
+  }
+
+  adicionaUsuario = () =>{
+    const novoUsuario = {
+      nomeUsuario: this.state.valorInputUsuario,
+      Mensagem: this.state.valorInputMensagem,
+      
+    };
+    
+
+    const novoPostagens = [...this.state.postagens, novoUsuario];
+
+    this.setState({postagens: novoPostagens})
+    this.setState({ valorInputUsuario: "" });
+    this.setState({ valorInputMensagem: "" });
+    
+  };
+
+  onChangeInputUsuario = (event) => {
+    this.setState({ valorInputUsuario: event.target.value})
+  }
+
+  onChangeInputMensagem = (event) => {
+    this.setState({ valorInputMensagem: event.target.value})
+  }
+  onKeyUp(event) {
+    if (event.charCode === 13) {
+      this.setState({ inputValue: event.target.value });
+    }
+}
+
+  
+
+  render() {
+
+    const postagens = this.state.postagens.map((usuario) =>{
+      return (
+        <div>
+          <b>{usuario.nomeUsuario}:</b> 
+          {usuario.Mensagem}
+          
+          </div>
+      );
+    });
+
+
+    return (
+      
+      <Container>
+        
+        <div>
+        <StyledInputs
+        value={this.state.valorInputUsuario}
+        onChange={this.onChangeInputUsuario}
+        placeholder={"Usuario"}/>
+
+        <StyledInputs1
+        value={this.state.valorInputMensagem}
+        onChange={this.onChangeInputMensagem}
+        placeholder={"Mensagem"}/>
+        
+        
+
+        <StyledButton onClick={this.adicionaUsuario}>Enviar</StyledButton>
+        </div>
+        <div>{postagens}</div>
+        
+        
+        
+      </Container>
+    );
+  }
 }
 
 export default App;
