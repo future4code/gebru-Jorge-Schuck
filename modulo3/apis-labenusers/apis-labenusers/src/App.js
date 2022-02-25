ba39e7ca331921e9b80a20354e59d7b83847c39e
JorgeSchuck <jorgegschuck@gmail.com>
2022-02-24 21:51:31 -0300

    enviando a pr exercicio apis-labenusers(inacabado)

---
+++ modulo3/apis-labenusers/apis-labenusers/src/App.js
@@ -0,0 +1,102 @@
+import React from "react";
+import axios from "axios";
+import styledComponents from "styled-components";
+
+
+const urlUser =
+  "https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users"
+
+const headers = {
+  headers: {
+    Authorization: "jorge-schuck-gebru"
+  }
+}
+
+export default class App extends React.Component {
+
+  state = {
+    users: [],
+    userInput: "",
+    emailInput: ""
+  };
+
+  componentDidMount() {
+    this.getAllUsers();
+  }
+  getAllUsers = () => {
+    axios
+      .get(urlUser, headers)
+      .then((res) => {
+        this.setState({ users: res.data })
+        // console.log(res.data.result)
+      })
+      .catch((err) => {
+        console.log(err.message)
+      })
+  }
+
+  createUser = () => {
+    const body = {
+      name: this.state.userInput,
+      email: this.state.emailInput
+    }
+    axios
+      .post(urlUser, body, headers)
+      .then((res) => {
+        console.log(res.data.result)
+
+
+
+        this.setState({ userInput: "" })
+        this.setState({ emailInput: "" })
+        alert("Usuário Cadastrado com Sucesso")
+
+        this.getAllUsers()
+          .catch((err) => {
+            console.log(err.res.data.message)
+            alert("Inserção Inválida")
+
+          })
+      })
+    this.setState({ userInput: "" })
+    this.setState({ emailInput: "" })
+
+  }
+  onUserTextChange = (event) => {
+    this.setState({ userInput: event.target.value });
+  };
+  onEmailTextChange = (event) => {
+    this.setState({ emailInput: event.target.value });
+  };
+
+  render() {
+    const usersComponents = this.state.users.map((novo) => {
+      return <li key={novo.id}> {novo.name}</li>
+    })
+
+    return (
+
+      <div>
+        <div>
+          <button>Trocar de Tela</button>
+        </div>
+        <input
+          value={this.state.userInput}
+          placeholder="Nome"
+          onChange={this.onUserTextChange}
+        />
+        <input
+          value={this.state.emailInput}
+          placeholder="Email"
+          onChange={this.onEmailTextChange}
+        />
+        <button onClick={this.createUser}>Criar Usuário</button>
+
+        <div>{usersComponents}</div>
+
+      </div>
+    )
+  }
+}
+
+
