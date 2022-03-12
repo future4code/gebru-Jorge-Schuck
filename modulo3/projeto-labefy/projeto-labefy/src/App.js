d20a1765ba7c6f83ee4110432d78af5e0c352081
JorgeSchuck <jorgegschuck@gmail.com>
2022-03-11 21:24:22 -0300

    Projeto Labefy concluído

--- modulo3/projeto-labefy/projeto-labefy/src/App.js
+++ modulo3/projeto-labefy/projeto-labefy/src/App.js
@@ -4,6 +4,39 @@ import axios from 'axios';
 import CreatePlaylistScreen from "./components/CreatePlaylistScreen"
 import PlaylistScreen from './components/PlaylistScreen';
 
+const EstiloBotoesPrincipais = styled.div`
+display:flex;
+justify-content:space-evenly;
+margin:30px;
+
+`
+const EstiloFundo = styled.div`
+
+align-items:center;
+justify-content:center;
+background-color:grey;
+background-image:url(https://h1.hqtexture.com/87/8657/1395012137-02.jpg);
+background-repeat:no-repeat;
+padding-bottom:30%;
+
+`
+const Botoes = styled.button`
+background-color: black;
+    border-radius:10px;
+    border: none;
+    color: white;
+    padding: 16px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    margin: 4px 2px;
+    -webkit-transition-duration: 0.4s;
+    transition-duration: 0.4s;
+    cursor: pointer;
+    font-family:Verdana, Geneva, Tahoma, sans-serif;
+    font-size:20px;
+`
 
 export default class App extends React.Component {
 
@@ -32,13 +65,13 @@ export default class App extends React.Component {
 
   render() {
     return (
-      <div>
-         <div>
-        <button onClick={this.goToPlaylistScreen}>PLAYLISTS</button>
-        <button onClick={this.goToCreatePlaylistScreen}>CRIE SUA PLAYLIST</button>
-         </div>
+      <EstiloFundo>
+         <EstiloBotoesPrincipais>
+         <Botoes onClick={this.goToCreatePlaylistScreen}>Faça sua Playlist</Botoes>
+         <Botoes onClick={this.goToPlaylistScreen}>Suas Playlists</Botoes>
+         </EstiloBotoesPrincipais>
         {this.choosePage()}
-      </div>
+      </EstiloFundo>
     )
   }
 }
--- modulo3/projeto-labefy/projeto-labefy/src/components/CreatePlaylistScreen.js
+++ modulo3/projeto-labefy/projeto-labefy/src/components/CreatePlaylistScreen.js
@@ -2,6 +2,37 @@ import React from "react";
 import axios from "axios";
 import styled from "styled-components";
 
+const EstiloTitulo = styled.h1`
+font-family:Arial, Helvetica, sans-serif ;
+font-size:80px ;
+`
+const EstiloContainer = styled.div`
+display:flex;
+flex-direction: column;
+justify-content:center;
+align-items:center;
+`
+const EstiloInput = styled.input`
+border-radius:7px ;
+height:20px;
+`
+const Botoes1 = styled.button`
+background-color: black;
+    border-radius:5px;
+    border: none;
+    color: white;
+    padding: 03px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    margin: 4px 2px;
+    -webkit-transition-duration: 0.4s;
+    transition-duration: 0.4s;
+    cursor: pointer;
+    font-family:Verdana, Geneva, Tahoma, sans-serif;
+    font-size:20px;
+`
 const url = "https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists"
 
 const headers = {
@@ -38,18 +69,19 @@ export default class CreatePlaylist extends React.Component {
     render(){
         return (
             <div>
-                <div>
-                    <h1>Labefy</h1>
-                    <h3>Crie uma Playlist</h3>
-                    <input
+                <EstiloContainer>
+                    <EstiloTitulo>Labefy</EstiloTitulo>
+                    <h3>Crie sua Playlist</h3>
+                    <h3>de forma rápida e fácil</h3>
+                    <EstiloInput
                     placeholder={"Nome da Playlist"}
                     value={this.state.playlist}
                     onChange={this.onChangeInputPlaylist}
                     />
-                    <button onClick={this.createPlaylist}>
-                        Criar Playlist
-                    </button>
-                </div>
+                    <Botoes1 onClick={this.createPlaylist}>
+                        Criar
+                    </Botoes1>
+                </EstiloContainer>
             </div>
         )
     }
